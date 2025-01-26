175f589fe2731bf0b7ab5d672d704008376b46fc
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-01-26 14:09:55 -0300

    finish `/api/v1/status` endoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js"
 
 async function  status (request, response){
-  const result = await database.query('SELECT 1 + 1 as sum')
-  console.log(result)
-  response.status(200).json({status: 'ok'})
+
+  const updatedAt = new Date().toISOString()
+  const postgresVersion = await database.query('SHOW server_version;')
+  const maxConnections = await database.query('SHOW max_connections;');
+
+  const dataBaseName = process.env.POSTGRES_DB
+
+  const connectionsUsed = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dataBaseName]
+  }); 
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies:{
+      database:{
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: connectionsUsed.rows[0].count
+      } 
+    }
+  })
 
 }
 
