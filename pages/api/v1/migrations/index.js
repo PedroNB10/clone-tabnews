cafb3c479b7490e4fd66aca2fca2cc9963ec6928
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-02-04 09:25:32 -0300

    fix opened connections with methods not allowed

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,22 +5,36 @@ import database from 'infra/database';
 
 export default async function  migrations (request, response){
   console.log('request.method', request.method)
-  const dbClient = await database.getNewClient()
-  const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: join('infra','migrations'),
-      verbose: true,
-      direction: 'up',
-      migrationsTable: 'pgmigrations'
+
+  const allowedMethods = ['GET', 'POST']
+
+  if (!allowedMethods.includes(request.method)){
+    return response.status(405).json({
+      error: `Method ${request.method} Not Allowed`,
+    })
+
   }
+
+
+  let dbClient
+try {
+
+  dbClient = await database.getNewClient()
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join('infra','migrations'),
+    verbose: true,
+    direction: 'up',
+    migrationsTable: 'pgmigrations'
+}
 
   if (request.method === 'GET'){
     console.log('ENTROU NO GET')
     const pendingMigrations = await migrationRunner(defaultMigrationOptions)
     console.log(pendingMigrations)
 
-    await dbClient.end()
   return response.status(200).json(pendingMigrations)
 
   }
@@ -32,7 +46,6 @@ export default async function  migrations (request, response){
       dryRun: false // para rodar em live run 
     })
     console.log(migratedMigrations)
-    await dbClient.end()
 
 
     if (migratedMigrations.length > 0) {
@@ -45,8 +58,17 @@ export default async function  migrations (request, response){
 
   }
 
-  
-  response.status(405).end()
+}
+
+catch (error){
+  console.log('error', error)
+  await dbClient.end()
+  response.status(500).json({error: error.message})
+}
+
+finally {
+  await dbClient.end()
+}
 
 }
 
