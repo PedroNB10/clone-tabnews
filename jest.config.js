a8f665e0721acfdab1216c657737cf21b7f06623
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2025-02-08 16:43:57 -0300

    makes npm test more robust with `async retry` and `orchestrator`

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const createJestconfig = nextJest()
 
 const jestConfig = createJestconfig({
   moduleDirectories: ['node_modules', '<rootDir>'],
+  testTimeout: 60000,
 })
 
 module.exports = jestConfig;
---
+++ tests/orchestrator.js
@@ -0,0 +1,28 @@
+import retry from 'async-retry'
+
+async function waitForAllServices(){
+    await waitForWebServer()
+
+    async function waitForWebServer(){
+
+        return retry(fechStatusPage, {
+            retries: 100,
+            maxTimeout: 1000,
+        })
+
+        async function fechStatusPage(bail, tryNumber){
+            console.log(`Attempt ${tryNumber}`)
+            const response = await fetch("http://localhost:3000/api/v1/status")
+
+            if (response.status !== 200){
+                throw Error()
+            }
+        }
+    }
+
+}
+
+export default
+{
+    waitForAllServices
+}
